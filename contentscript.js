29ecc53729ba057ff41139ec2b5f428fa3a29ee6
Peter Schoeffman <schoeffman@gmail.com>
2015-04-15 13:17:11 -0400

    this context fixes

--- contentscript.js
+++ contentscript.js
@@ -8,17 +8,18 @@ function load(data) {
     
     var pizzaTracker = {
       prevState : null,
-      channel   : channel, 
-      token     : token,
+      channel   : null, 
+      token     : null,
       notify    : function(state) {
         var n = new XMLHttpRequest();
-        n.open("POST", "https://slack.com/api/chat.postMessage?token=" + this.token + "&channel=%23" + this.channel + "&text=" + state + "&username=Domino's%20Notify&icon_url=http%3A%2F%2Fcdn.marketplaceimages.windowsphone.com%2Fv8%2Fimages%2F81c41161-1148-4f50-bdda-dd681d2a4b1d%3FimageType%3Dws_icon_large&pretty=1");
+        n.open("POST", "https://slack.com/api/chat.postMessage?token=" + pizzaTracker.token + "&channel=%23" + pizzaTracker.channel + "&text=" + state + "&username=Domino's%20Notify&icon_url=http%3A%2F%2Fcdn.marketplaceimages.windowsphone.com%2Fv8%2Fimages%2F81c41161-1148-4f50-bdda-dd681d2a4b1d%3FimageType%3Dws_icon_large&pretty=1");
         n.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         n.send();
     },
-      checkStatus : function() {
+    checkStatus : function() {
 
         var state = '';
+        var message = '';
 
         try {
          state = document.getElementById('intl-tracker').getAttribute('data-currentstage');
@@ -45,7 +46,7 @@ function load(data) {
             }
           }
      
-          notify(message, token, channel);
+          notify(message);
       }
     }; //end pizzaTracker
 
